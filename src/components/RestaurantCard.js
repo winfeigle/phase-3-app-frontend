b2c84dcf723c59016b8df8628786fac9504b66e5
Win Feigle <win.feigle@gmail.com>
2022-11-27 16:16:44 -0700

    Add all elements to RestaurantCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,12 +1,21 @@
-import React, {useState} from "react";
+import React from "react";
 
 function RestaurantCard({restaurant}){
-    const {id, name, address, tag} = restaurant;
+    // const [mealPlans, setMealPlans] = useState([]);
+    const {id, name, address, tag, logo_url} = restaurant;
+
 
     return(
-        <div className="card">
-            <h2><b>{name}</b></h2>
+        <div className="restaurant-card">
             <p className="tag">{tag}</p>
+            <div className="restaurant-logo-container">
+                <img className="restaurant-logo" src={logo_url} alt="Restaurant logo"/>
+                <h2><b>{name}</b></h2>
+            </div>
+            <div className="restaurant-image">
+                {/* <img> PLACE IMAGE HERE </img> */}
+            </div>
+            <p className="address">{address}</p>
         </div>
     );
 
