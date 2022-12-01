b8395198adc1cdbcd9fd91cf210d2eac6bf78113
Win Feigle <win.feigle@gmail.com>
2022-12-01 15:12:18 -0700

    Fix CSS styles

--- src/components/MealPlans.js
+++ src/components/MealPlans.js
@@ -6,19 +6,24 @@ function MealPlans({mealPlans}){
     const renderMealplan = mealPlans.map(plan => {
         const {id, name, number_of_meals, price } = plan;
         return(
-            <div key={id} className="mealplan-card">
-                <div className="mealplan-price">
-                    <b>{`$${price}.00`}</b>
+            <div key={id} className="mealplan">
+                <div className="mealplan-card">
+                    <div className="mealplan-price">
+                        <b>{`$${price}.00`}</b>
+                    </div>
+                    <div className="mealplan-info">
+                        <span className="mealplan-name">
+                            {`${name} Feedplan`}
+                        </span>
+                        <span className="mealplan-meals">
+                            {`${number_of_meals} meals`}
+                        </span>
+                    </div>
                 </div>
-                <div className="mealplan-info">
-                    <span className="mealplan-name">
-                        {`${name} Feedplan`}
-                    </span>
-                    <span className="mealplan-meals">
-                        {`${number_of_meals} meals`}
-                    </span>
+                
+                <div className="mealplan-subscribe">
+                    <button>Subscribe</button>
                 </div>
-
             </div>
         )
     })
